4d9ff8b8ab1d7b53defd36ec4b835619ee6cb2e2
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2012-10-05 08:46:30 +0100

    first_or_create with two hashes

--- zoo.rb
+++ zoo.rb
@@ -81,5 +81,49 @@ end
 
 zoo = Zoo.first_or_create(:name => 'The Glue Factory')
 
+# This will first try to find a Zoo instance with the given 
+# name, and if it fails to do so, it will return a newly 
+# created Zoo with that name.
+
 puts "zoo.saved? = #{zoo.saved?}"
 puts "zoo = #{zoo.inspect}"
+
+# If the criteria we want to use to query for the resource 
+# differ from the attributes we need for creating a new 
+# resource, we can pass the attributes for creating a new 
+# resource as the second parameter to #first_or_create, 
+# also in the form of a #Hash:
+
+zoo = Zoo.first_or_create({ :name => 'The Glue Factory' }, 
+                          { :inception => Time.now })
+
+# This will search for a Zoo named 'The Glue Factory' and if 
+# it can't find one, it will return a new Zoo instance with 
+# its name set to 'The Glue Factory' and the inception set to 
+# what has been Time.now at the time of execution. 
+# You can see that for creating a new resource, both hash 
+# arguments will be merged so we don't need to specify the 
+# query criteria again in the second argument Hash that lists 
+# the attributes for creating a new resource. 
+
+puts "zoo.saved? = #{zoo.saved?}"
+puts "zoo = #{zoo.inspect}"
+
+# However, if we really need to create 
+# the new resource with different values 
+# from those used to query for it, the second Hash argument 
+# will overwrite the first one.
+
+zoo = Zoo.first_or_create({ :name => 'The Chocolat Factory' }, {
+  :name      => 'Brooklyn Zoo',
+  :inception => Time.now
+})
+
+# This will search for a Zoo named 'The Chocolat Factory' but 
+# if it fails to find one, it will return a Zoo instance 
+# with its name set to 'Brooklyn Zoo' and its inception set 
+# to the value of Time.now at execution time.
+
+puts "zoo.saved? = #{zoo.saved?}"
+puts "zoo = #{zoo.inspect}"
+
